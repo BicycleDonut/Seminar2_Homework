872c011a917b60ce844fdd15d1a63052b86aafd9
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-12 09:45:52 +0300

    add T10

---
+++ T10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на
+// выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+string N = new Random().Next(100, 1000).ToString();
+Console.WriteLine($"Число: {N}");
+int second = int.Parse(N[N.Length - 2].ToString());
+Console.WriteLine($"Вторая цифра числа: {second}");
+
